c5dfc84f5cab17b4bd2b6d82338a107df4114b26
levi3609 <nazmulhasan3609@gmail.com>
2024-04-01 16:19:11 +0600

    Revert "Alioth: los bringup"
    This reverts commit cef510479d5d501e9b33615d58c12bb205a37584.

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -10,15 +10,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common VoidUI stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-# Google Basic apps
-$(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
-
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := aosp_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -37,8 +34,7 @@ PRODUCT_USE_SCUDO := true
 
 # Rom Specific Flags
 TARGET_INCLUDE_WIFI_EXT := true
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_FACE_UNLOCK_SUPPORTED := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_SUPPORTS_QUICK_TAP  := true
-
+TARGET_SUPPORTS_QUICK_TAP  := false
